4400a7bcd00fa660f9a19b2f25f8b6a7259c05b8
Julian Popescu <jpopesculian@gmail.com>
2022-02-26 17:17:07 +0100

    b17 iter

--- src/lib.rs
+++ src/lib.rs
@@ -40,22 +40,51 @@ impl B17 {
     }
 }
 
-// struct B17Iter<'a> {
-//     data: &'a [u8],
-//     index: usize,
-//     bit_offset: usize,
-// }
+struct B17Iter<'a> {
+    data: &'a [u8],
+    index: usize,
+    bit_offset: usize,
+}
 
-// impl<'a> Iterator for B17Iter<'a> {
-//     type Item = B17;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if self.index >= self.data.len() {
+impl<'a> B17Iter<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        Self {
+            data,
+            index: 0,
+            bit_offset: 0,
+        }
+    }
+}
 
-//         }
-//         let mut res = 0;
-//         res ||= self.data[self.index] << self.bit_offset;
-//     }
-// }
+impl<'a> Iterator for B17Iter<'a> {
+    type Item = B17;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.data.len() {
+            return None;
+        }
+        let mut res: u32 = 0;
+        res |= ((self.data[self.index] << self.bit_offset) as u32) << 9;
+
+        self.index += 1;
+        if self.index >= self.data.len() {
+            return Some(B17(res));
+        }
+        res |= (self.data[self.index] as u32) << (1 + self.bit_offset);
+
+        self.index += 1;
+        if self.index >= self.data.len() {
+            return Some(B17(res));
+        }
+        res |= (self.data[self.index] >> (7 - self.bit_offset)) as u32;
+
+        self.bit_offset += 1;
+        if self.bit_offset > 7 {
+            self.index += 1;
+            self.bit_offset = 0;
+        }
+        Some(B17(res))
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -78,5 +107,31 @@ mod tests {
                 B17(test_case)
             )
         }
+    }
+
+    #[test]
+    fn b17_iter() {
+        const BYTES: &[u8] = &[
+            0b0001_0001,
+            0b0010_0010,
+            0b0011_0011,
+            0b0100_0100,
+            0b0101_0101,
+            0b0110_0110,
+            0b0111_0111,
+            0b1000_1000,
+        ];
+        assert_eq!(
+            B17Iter::new(BYTES).into_iter().collect::<Vec<_>>(),
+            vec![
+                #[allow(clippy::unusual_byte_groupings)]
+                B17(0b0001_0001_0010_0010_0),
+                B17(0b011_0011_0100_0100_01),
+                #[allow(clippy::unusual_byte_groupings)]
+                B17(0b01_0101_0110_0110_011),
+                #[allow(clippy::unusual_byte_groupings)]
+                B17(0b1_0111_1000_1000_0000)
+            ]
+        )
     }
 }
